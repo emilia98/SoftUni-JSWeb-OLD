1e05bf51f28c51ff909722453c0e282bf3f17852
emilia98 <emilia_nedialkova@abv.bg>
2018-06-02 14:06:58 +0300

    some modifications

--- ExpressJS Fundamentals/05. NodeJS Web Server & Dev.Tools - Exercise/MovieDB/handlers/favicon-handler.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = (req, res) => {
-    if(req.pathname.endsWith === '.ico')
-}
--- ExpressJS Fundamentals/05. NodeJS Web Server & Dev.Tools - Exercise/MovieDB/handlers/index.js
+++
@@ -1,4 +0,0 @@
-const homeHandler = require('./home.js');
-const staticFilesHandler = require('./static-files.js');
-
-module.exports = [homeHandler, staticFilesHandler];
